2512fc5006a1b1245d013bb20a2f2832853bf9d4
liangv21 <liang.va@northeastern.edu>
2023-02-27 11:25:14 -0500

    first scatter

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// Set information for frame height/width/margins
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 500;
+const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+
+// Set vis height/width
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+
+// Set color mapping for graphs
+const color = {"setosa": "royalblue",
+                "versicolor": "seagreen",
+                "virginica": "salmon"};
+
+// Build frame of scatterplot1
+const FRAME1 = d3.select("#scatter1")
+                  .append("svg")
+                    .attr("width", FRAME_WIDTH)
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("class", "frame");
+
+// Load the data from the CSV file
+d3.csv("data/iris.csv").then((data) => {
+
+    // Create the scatterplot points
+    FRAME1.selectAll("points")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("class", "point")
+            // calculations for x: increases scale to be visible to user + margin adjustment
+            .attr("cx", (d) => { return (parseFloat(d.Sepal_Length) * 50) + MARGINS.left; })
+            // calculations for y: increases scale, considers our perception of x/y plane, margins
+            .attr("cy", (d) => { return ((8 - parseFloat(d.Petal_Length)) * 50) + MARGINS.top; })
+            .attr("fill", function(d,i) { console.log(d.Species, color[d.Species]); return color[d.Species]; })
+            .attr("r", 4)
+            .attr("opacity", 0.5);
+
+    // Add axis to the graph: establish scale functions for x/y
+    const X_AXIS_SCALE = d3.scaleLinear()
+    .domain([0, 8])
+    .range([0, VIS_WIDTH]);
+
+    const Y_AXIS_SCALE = d3.scaleLinear()
+    .domain([0, 8])
+    .range([VIS_HEIGHT, 0]);
+
+    // Add x-axis
+    FRAME1.append("g")
+    .attr("transform", "translate(" + MARGINS.left + 
+    "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+    .call(d3.axisBottom(X_AXIS_SCALE).ticks(8)) 
+    .attr("font-size", "10px"); 
+
+    // Add y-axis
+    FRAME1.append("g")
+    .attr("transform", "translate(" + MARGINS.left +
+    "," + MARGINS.top + ")")
+    .call(d3.axisLeft(Y_AXIS_SCALE).ticks(8))
+    .attr("font-size", "10px");
+  
+});
